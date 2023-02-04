0f5baeb1d114eef756a060efd04c1ceb0e618dd5
Prabhat Mani Srivastava <prabhatmani933@gmail.com>
2023-02-05 03:42:50 +0530

    Deploying to vercel.

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBONRW3UD9gESjLTgrR1xOYERZG0CabcfM",
-  authDomain: "whatsapp-clone-33949.firebaseapp.com",
-  projectId: "whatsapp-clone-33949",
-  storageBucket: "whatsapp-clone-33949.appspot.com",
-  messagingSenderId: "217830503777",
-  appId: "1:217830503777:web:2361b0d69998e2a17bc120",
-  measurementId: "G-50V7KF1EZF",
+  apiKey: "AIzaSyC8jab8C7AMlUUV0FFS5lAeZB-INAo3EnI",
+  authDomain: "whatsapp-47f3a.firebaseapp.com",
+  projectId: "whatsapp-47f3a",
+  storageBucket: "whatsapp-47f3a.appspot.com",
+  messagingSenderId: "343408589986",
+  appId: "1:343408589986:web:67d9d4a9f16656ea146bfb",
+  measurementId: "G-GGDRQ6NJ6F",
 };
 
 const app = initializeApp(firebaseConfig);
