2c4d1bed68262b5821fc22f6434bc1dee9d992f9
Prabhat Mani Srivastava <prabhatmani933@gmail.com>
2023-02-11 19:20:34 +0530

    Updated firebase configuration.

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC8jab8C7AMlUUV0FFS5lAeZB-INAo3EnI",
-  authDomain: "whatsapp-47f3a.firebaseapp.com",
-  projectId: "whatsapp-47f3a",
-  storageBucket: "whatsapp-47f3a.appspot.com",
-  messagingSenderId: "343408589986",
-  appId: "1:343408589986:web:67d9d4a9f16656ea146bfb",
-  measurementId: "G-GGDRQ6NJ6F",
+  apiKey: "AIzaSyArjDAPfREvmR9yHvwewX_A_RtJkoTsPzo",
+  authDomain: "chatsapp-bfb32.firebaseapp.com",
+  projectId: "chatsapp-bfb32",
+  storageBucket: "chatsapp-bfb32.appspot.com",
+  messagingSenderId: "583658933592",
+  appId: "1:583658933592:web:b385b340938399561ea92d",
 };
 
 const app = initializeApp(firebaseConfig);
